3042a2cc056c224c6c5c035de5e2fc2a6be9190f
DTProjects <dima.tsaritsyn@gmail.com>
2023-09-28 16:20:54 -0700

    Basic customer list up, minor style fixes

---
+++ Orders.Web/Controllers/CustomerController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Orders.DAL;
+using Orders.Web.Models;
+
+namespace Orders.Web.Controllers
+{
+	public class CustomerController : Controller
+	{
+		private readonly IOrdersRepository _repository;
+		public CustomerController(IOrdersRepository repository)
+		{
+			_repository = repository;
+		}
+		[HttpGet]
+		public IActionResult Customers()
+		{
+			IEnumerable<CustomerModel> model = _repository.GetCustomers().Select(c=>new CustomerModel(c));
+			return View(model);
+		}
+	}
+}
