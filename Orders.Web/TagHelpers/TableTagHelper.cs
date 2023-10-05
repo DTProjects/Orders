7a17d358c479713478cfc4b8d177a937e0c58cdc
DTProjects <dima.tsaritsyn@gmail.com>
2023-10-04 17:42:39 -0700

    Added tag helpers for order table

---
+++ Orders.Web/TagHelpers/TableTagHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Orders.Web.TagHelpers
+{
+	[HtmlTargetElement("table")]
+	public class TableTagHelper : TagHelper
+	{
+		public string TblProps { get; set; } = "table table-striped table-hover";
+
+		public override void Process(TagHelperContext context, TagHelperOutput output)
+		{
+			output.TagName = "table";
+			output.Attributes.Add("class", TblProps);
+
+
+			base.Process(context, output);
+		}
+	}
+}
